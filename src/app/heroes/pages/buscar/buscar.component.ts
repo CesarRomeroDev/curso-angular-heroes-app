f01251f9feceea082aef76b4eed948b65c248e06
CesarRomeroDev <cesar_romero.dev@outlook.com>
2021-09-08 18:00:09 -0500

    MaterialAutocomplete

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { ChildActivationStart } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interfaces';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-buscar',
@@ -8,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  heroes: Heroe[] = [];
+
+  constructor( private HeroesService: HeroesService ) { }
 
   ngOnInit(): void {
+  }
+
+  buscando() {
+
+    this.HeroesService.getSugerencias( this.termino ).subscribe( heroes => this.heroes = heroes );
+
+  }
+
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent ){
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
   }
 
 }
