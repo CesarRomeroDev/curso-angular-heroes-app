77e5a3a586feabdb022fc89edde235f968cfec85
CesarRomeroDev <cesar_romero.dev@outlook.com>
2021-09-08 13:39:35 -0500

    HeroeTarjetaComponent

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { MatIconModule } from '@angular/material/icon';
       MatSidenavModule,
       MatToolbarModule,
       MatButtonModule,
-      MatIconModule
+      MatIconModule,
+      MatListModule,
+      MatCardModule
   ]
 })
 export class MaterialModule { }
