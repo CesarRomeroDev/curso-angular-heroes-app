2ab9375d2cf5dc1bb9475da9f3f56fcbb1506c11
CesarRomeroDev <cesar_romero.dev@outlook.com>
2021-09-09 08:45:10 -0500

    finDeLaSeccion13HayQueContinuarConLaSeccion14

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -15,6 +15,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe | undefined;
 
   constructor( private HeroesService: HeroesService ) { }
 
@@ -23,13 +24,21 @@ export class BuscarComponent implements OnInit {
 
   buscando() {
 
-    this.HeroesService.getSugerencias( this.termino ).subscribe( heroes => this.heroes = heroes );
+    this.HeroesService.getSugerencias( this.termino.trim() ).subscribe( heroes => this.heroes = heroes );
 
   }
 
   opcionSeleccionada( event: MatAutocompleteSelectedEvent ){
+
+    if (!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return
+    }
+
     const heroe: Heroe = event.option.value;
     this.termino = heroe.superhero;
+
+    this.HeroesService.getHeroePorId( heroe.id! ).subscribe( heroe => this.heroeSeleccionado = heroe );
   }
 
 }
